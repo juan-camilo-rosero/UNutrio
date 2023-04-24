f6d94ddb53ff7bc1089cde4d8b92292ef57edcd4
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2023-04-24 17:11:13 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,30 @@
+const d = document
+
+export function selectNav (btn, bar, title) {
+    const $navBtns = d.querySelectorAll(btn),
+    $bar = d.querySelector(bar),
+    $title = d.querySelector(title)
+
+    $navBtns.forEach((btn, index) => {
+        btn.addEventListener("click", e => {
+            $title.textContent = btn.textContent
+            $navBtns.forEach(el => el.classList.remove("green"));
+            console.log(btn.classList);
+            btn.classList.add("green")
+            switch (index) {
+                case 0:
+                    $bar.style.marginTop = "0vh"
+                    break;
+                case 1:
+                    $bar.style.marginTop = "12.5vh"
+                    break;
+                case 2:
+                    $bar.style.marginTop = "25vh"
+                    break;
+                case 3:
+                    $bar.style.marginTop = "37.5vh"
+                    break;
+            }
+        })
+    });
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+import { selectNav } from "./nav.js"
+
+const d = document
+
+selectNav(".options a", ".nav-bar-selected", "#tool h2")
